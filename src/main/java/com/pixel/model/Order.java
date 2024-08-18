179b3541144c68da0d645cac8ca8b93fb8e551c2
sekharjpapp <43300292+sekharjpapp@users.noreply.github.com>
2024-08-18 15:41:19 +0530

    init msg

---
+++ src/main/java/com/pixel/model/Order.java
@@ -0,0 +1,16 @@
+package com.pixel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private String orderId;
+    private String laptop;
+    private int quantity;
+
+}
